265214a907b803cf17825e19a44b6beb885238b3
Frank Ngo <Frankngomusic@gmail.com>
2018-04-01 22:22:41 -0700

    add display ino for first and last name of doctor:

--- src/DoctorLookup.js
+++ src/DoctorLookup.js
@@ -5,8 +5,8 @@ export class doctorApi {
   getDoctors(userInput, displayDoctor) {
     const apiKey = process.env.exports.apiKey;
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${userInput}&location=or-portland&skip=0&limit=10&user_key=${apiKey}`)
-    .then(response => {
-      displayDoctor(response);
+    .then(entry => {
+      displayDoctor(entry);
     })
     .fail(() => {
       $('#error').text("An error has occured.")
--- src/main.js
+++ src/main.js
@@ -3,6 +3,22 @@ import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { doctorApi } from './DoctorLookup.js';
 
+const displayDoctor = entry => {
+  if (response.data.length == 0 ) {
+    $('.list-doctors').text("There is no match with your search")
+  } else {
+    response.data.forEach(data => {
+      let firstName = data.profile.first_name;
+      let lastName = data.profile.last_name;
+
+      $('#doctors').append(`<li>`
+        + firstName + " "
+        + lastName + " " + `<br>`
+    })
+  }
+}
+
+
 $(document).ready(function() {
   $('#searchDoctor').click(e => {
   e.preventDefault();
