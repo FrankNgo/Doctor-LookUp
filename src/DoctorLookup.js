228934fd5b1939f8a1f764db8536d25830a65dc6
Frank Ngo <Frankngomusic@gmail.com>
2018-04-01 22:41:18 -0700

    add api key

--- src/DoctorLookup.js
+++ src/DoctorLookup.js
@@ -3,7 +3,8 @@ import $ from 'jquery';
 export class doctorApi {
 
   getDoctors(userInput, displayDoctor) {
-    const apiKey = process.env.exports.apiKey;
+    // const apiKey = process.env.exports.apiKey;
+    const apiKey = '7c9ea3b81dc4d6d7f0c88e345d77cae5'
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${userInput}&location=or-portland&skip=0&limit=10&user_key=${apiKey}`)
     .then(entry => {
       displayDoctor(entry);
