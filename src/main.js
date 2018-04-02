03beb8e735de32fea1eeb5c84dfb86e14cfa960b
Frank Ngo <Frankngomusic@gmail.com>
2018-04-01 23:43:02 -0700

    add better layout for display

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { doctorApi } from './DoctorLookup.js';
+import {
+  doctorApi
+} from './DoctorLookup.js';
 
 const displayDoctor = entry => {
-  if (entry.data.length == 0 ) {
+  if (entry.data.length == 0) {
     $('.doctorsList').text("There is no match with your search")
   } else {
     entry.data.forEach(data => {
@@ -19,28 +21,26 @@ const displayDoctor = entry => {
       let doctorWebsite = data.practices[0].website;
       if (doctorWebsite === undefined) {
         doctorWebsite = "No website available."
-      }
-      else if (doctorWebsite === entry.data.practices[0].website ){
+      } else if (doctorWebsite === entry.data.practices[0].website) {
         doctorWebsite = entry.data.practices[0].website;
       }
       let acceptNewPatients = data.practices[0].accepts_new_patients;
       if (acceptNewPatients === true) {
-          acceptNewPatients = "Currently accepting new patients.";
-       }
-       else if (acceptNewPatients === false) {
+        acceptNewPatients = "Currently accepting new patients.";
+      } else if (acceptNewPatients === false) {
         acceptNewPatients = "Not currently accepting new patients."
-       }
+      }
 
-      $('#showDoctors').append(`<li>`+ firstName + " "+ lastName + " " + address_street + " " + address_state + " " + address_zip + " " + phoneNumber + " " + doctorWebsite + " " + acceptNewPatients + " " + biography + " " + `<img src ="` + picture + `">` + `<br>`);
+      $('#showDoctors').append("</p> </div> <div class='col-lg-5 col-sm-pull-6  col-sm-6'><img class='img-responsive doc-profile' src=" + picture + "<br>" + "<br>" + firstName + ' ' + lastName + "<br>" + address_street + ' ' + address_state + ' ' + address_zip + "<br>" + phoneNumber + "</h2>" + "<p class='lead'>" + biography + "<br>" + doctorWebsite + "<br>" +  "<strong>" + acceptNewPatients);
     });
   }
 }
 
 $(document).ready(function() {
   $('#searchDoctor').click(e => {
-  e.preventDefault();
-  let userInput = $('#doctorFind').val();
-  doctorApi.prototype.getDoctors(userInput, displayDoctor)
-  $('#showDoctors').empty();
+    e.preventDefault();
+    let userInput = $('#doctorFind').val();
+    doctorApi.prototype.getDoctors(userInput, displayDoctor)
+    $('#showDoctors').empty();
   });
 });
