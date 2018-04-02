6a03b25ffe58e1fc762a89cd9328238b80d487df
Frank Ngo <Frankngomusic@gmail.com>
2018-04-01 22:16:37 -0700

    add basic landing page

---
+++ src/DoctorLookup.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+
+export class doctorApi {
+
+  getDoctors(userInput, displayDoctor) {
+    const apiKey = process.env.exports.apiKey;
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${userInput}&location=or-portland&skip=0&limit=10&user_key=${apiKey}`)
+    .then(response => {
+      displayDoctor(response);
+    })
+  }
+}
